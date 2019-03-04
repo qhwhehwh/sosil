a50f97cd1194673847d82fdfff3f862fc23cddf9
hans <qhwhehwh@likelion.org>
2019-03-04 18:40:11 +0900

    e

--- sosil/settings.py
+++ sosil/settings.py
@@ -129,3 +129,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = ['https://hansosil.herokuapp.com/','127.0.0.1']
